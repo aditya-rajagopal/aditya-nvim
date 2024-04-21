b9b37bd11747c9862dae9080efec52c6664bbd16
Aditya Rajagopal <adi.raj465@gmail.com>
2024-04-20 18:16:25 -0700

    add hotkeys

--- lua/aditya/core/remap.lua
+++ lua/aditya/core/remap.lua
@@ -10,11 +10,23 @@ vim.opt.signcolumn = 'yes'
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+vim.opt.wrap = false
+vim.opt.swapfile = false
+
 -- vim.opt.cursorline = true
 
 vim.opt.scrolloff = 12
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
 
 vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "120"
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Expand a split and bring it back
@@ -22,6 +34,20 @@ vim.keymap.set('n', '<leader>=', '<C-w>=')
 vim.keymap.set('n', '<leader>|', '<C-w>|')
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
@@ -32,38 +58,45 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 ---- Use LspAttach autocommand to only map the following keys
 ---- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('AdityaLspConfig', {}),
-	callback = function(ev)
-		-- Enable completion triggered by <c-x><c-o>
-		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    group = vim.api.nvim_create_augroup('AdityaLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local opts = { buffer = ev.buf }
-		vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, opts)
-		vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set('n', '<leader>gh', vim.lsp.buf.hover, opts)
-		vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-		vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-		vim.keymap.set('n', '<leader>wl', function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, opts)
-		vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-		vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-		vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', '<leader>f', function()
-			vim.lsp.buf.format { async = true }
-		end, opts)
-	end,
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        local builtin = require("telescope.builtin")
+        vim.keymap.set('n', 'gD', builtin.lsp_definitions, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gh', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations, opts)
+        vim.keymap.set('n', '<leader>D', builtin.lsp_type_definitions, opts)
+        vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, opts)
+        vim.keymap.set('n', '<leader>ws', builtin.lsp_dynamic_workspace_symbols, opts)
+        vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, opts)
+
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>gr', builtin.lsp_references, opts)
+        vim.keymap.set('n', '<leader>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
 })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking text',
-	group = vim.api.nvim_create_augroup('aditya-highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('aditya-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
--- lua/aditya/plugins/telescope.lua
+++ lua/aditya/plugins/telescope.lua
@@ -1,13 +1,21 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
-	end
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>pws', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>pWs', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+    end
 }
