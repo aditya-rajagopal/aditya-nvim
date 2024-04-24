14dbe93594b6b7cfda75bc44eb714f27bacfff21
Aditya Rajagopal <adi.raj465@gmail.com>
2024-04-23 17:27:48 -0700

    change to rosepine but add muted colours to cpp comments

--- lua/aditya/plugins/colours.lua
+++ lua/aditya/plugins/colours.lua
@@ -9,51 +9,54 @@ function ColorMyPencils(color)
     vim.cmd 'highlight TelescopeTitle guibg=none'
 end
 
--- return {
---     {
---         "rose-pine/neovim",
---         name = "rose-pine",
---         config = function()
---             require('rose-pine').setup({
---                 disable_background = true,
---                 styles = {
---                     italic = false,
---                 },
---             })
---             vim.cmd("colorscheme rose-pine")
---             ColorMyPencils()
---         end
---     }
--- }
-
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-        vim.cmd [[colorscheme tokyonight-night]]
-        require('tokyonight').setup({
-            style = 'night',
-            styles = {
-                comments = { italics = true },
-                keywords = { italics = true },
-                functions = {},
-                variables = {},
-                sidebars = "dark",
-                floats = "dark",
-            },
-            transparent = false,
-            terminal_colours = true,
-        })
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-        vim.cmd 'highlight TelescopeBorder guibg=none'
-        vim.cmd 'highlight TelescopeTitle guibg=none'
-        vim.cmd 'highlight SignColumn guibg=none'
-    end
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require('rose-pine').setup({
+                disable_background = true,
+                styles = {
+                    italic = false,
+                },
+                highlight_groups = {
+                    ['@lsp.type.comment.cpp'] = { fg = 'muted' },
+                }
+            })
+            vim.cmd("colorscheme rose-pine")
+            ColorMyPencils()
+        end
+    }
 }
+
+-- return {
+--     "folke/tokyonight.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {},
+--     config = function()
+--         vim.cmd [[colorscheme tokyonight-night]]
+--         require('tokyonight').setup({
+--             style = 'night',
+--             styles = {
+--                 comments = { italics = true },
+--                 keywords = { italics = true },
+--                 functions = {},
+--                 variables = {},
+--                 sidebars = "dark",
+--                 floats = "dark",
+--             },
+--             transparent = false,
+--             terminal_colours = true,
+--         })
+--         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--         vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+--         vim.cmd 'highlight TelescopeBorder guibg=none'
+--         vim.cmd 'highlight TelescopeTitle guibg=none'
+--         vim.cmd 'highlight SignColumn guibg=none'
+--     end
+-- }
 
 --return {
 --	"bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000,
